727c89c54aadad27047af3efcde52e4793f6868c
Tejas Kumar <tejas@contiamo.com>
2018-05-03 21:47:27 +0200

    Rough second draft

--- src/App.js
+++ src/App.js
@@ -1,82 +1,102 @@
-import React, { Component } from "react"
-import { Document, Page } from "react-pdf"
+import React, { Component } from "react";
+import { Document, Page } from "react-pdf";
+import "./App.css";
 
 class App extends Component {
-  state = {
-    loading: true,
-  }
-
   onDocumentLoad = () => {
-    this.setState(() => ({ loading: false }))
-  }
+    this.setState(() => ({ loading: false }));
+  };
 
   render() {
-    const { loading } = this.state
     return (
-      <div style={{ display: "grid", gridTemplateColumns: "320px 768px" }}>
-        {loading && (
-          <div className="loader-container">
-            <div className="loader" />
+      <div>
+        <section className="logo-container">
+          <img alt="The Limelight Collective" src="/logo.jpg" />
+        </section>
+        <section className="upcoming-events">
+          <h2 className="upcoming-events__heading">Kommende Veranstaltungen</h2>
+          <div className="upcoming-events__grid">
+            <div className="event">
+              <h3 className="event__heading">A Third of the Stars</h3>
+              <small>06.06.2018 - 10.06.2018</small>
+              <img
+                className="event__image"
+                alt="A Third of the Stars"
+                src="/third.png"
+              />
+
+              <a
+                className="button"
+                href="https://performingarts-festival.tickets.de/en/tour/10750-a_third_of_the_stars_the_limelight_collective_"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Tickets Kaufen
+              </a>
+              <p>
+                Das Tanzstück „A Third of the Stars“ setzt sich mit universalen
+                Gut-gegen-Böse-Handlungen auf der geistlichen und
+                gesellschaftlichen Ebene und auf der Verhaltensebene
+                auseinander. Mystisch erzählt, erinnert es an eine epische
+                Legende und lädt die Zuschauer*innen ein, Platz in einer
+                Geschichte zu finden, die größer ist, als sie selbst. Im Zentrum
+                der Arbeit von The Limelight Collective steht die Spiritualität.
+              </p>
+            </div>
+            <div className="event">
+              <h3 className="event__heading">In Der Strafkolonie</h3>
+              <small>06.06.2018 - 10.06.2018</small>
+              <img
+                className="event__image"
+                alt="In Der Strafkolonie"
+                src="/straf.png"
+              />
+
+              <a
+                className="button"
+                href="https://performingarts-festival.tickets.de/de/tour/10778-1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Tickets Kaufen
+              </a>
+              <p>
+                Eine Forschungsreisende findet sich in einer Strafkolonie wieder
+                und wird gebeten der Exekution eines Soldaten beizuwohnen, der
+                wegen Ungehorsam und Beleidigung verurteilt wurde. Eine fein
+                ausgetüftelte Maschine führt zur Frage über Recht und Unrecht.
+                Was nennen wir Zivilisation? Welche Verantwortung ergreifen wir?
+                Heuwinkel und Steffen arbeiten freischaffend als Regisseur bzw.
+                Schauspielerin.
+              </p>
+            </div>
           </div>
-        )}
-        <div style={{ padding: "0 32px" }}>
-          <div>
-            <h2>Upcoming Events</h2>
-            <ul className="events">
-              <li className="event">
-                <h3>Soul Space</h3>
-                <small>30 Apr 2018, 19:30</small>
-                <div>
-                  <button>Join</button>
-                </div>
-              </li>
-              <li className="event">
-                <h3>Open Stage</h3>
-                <small>4 May 2018, 19:30</small>
-                <div>
-                  <button>Join</button>
-                </div>
-              </li>
-              <li className="event">
-                <h3>Soul Space</h3>
-                <small>7 Apr 2018, 19:30</small>
-                <div>
-                  <button>Join</button>
-                </div>
-              </li>
-              <li className="event">
-                <h3>Soul Space</h3>
-                <small>14 May 2018, 19:30</small>
-                <div>
-                  <button>Join</button>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div style={{ marginTop: 64 }}>
-            <h2>A Third of the Stars</h2>
-            <img
-              src="https://images.pexels.com/photos/209948/pexels-photo-209948.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-              style={{ width: "100%" }}
-            />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tellus ullamcorper, rutrum tellus sed,
-              sodales nibh. Duis convallis tincidunt lacus, id laoreet ante mollis tincidunt. Quisque feugiat sit amet
-              tortor id sollicitudin. Morbi sit amet sem tellus. Nulla convallis dapibus scelerisque. In fringilla,
-              tortor volutpat egestas tristique, lectus neque blandit augue, ac auctor orci arcu in ipsum.
-            </p>
-            <button>Buy Tickets</button>
-          </div>
-        </div>
-        <div style={{ height: "100vh", overflow: "auto", WebkitOverflowScrolling: "touch" }}>
+        </section>
+        <section className="flyer">
           <Document file="broc.pdf" onLoadSuccess={this.onDocumentLoad}>
             <Page pageNumber={1} />
             <Page pageNumber={2} />
           </Document>
-        </div>
+        </section>
+        <section className="footer">
+          <div className="footer__logo-container">
+            <img className="footer__logo" alt="Heilsarmee" src="/salvos.jpg" />
+          </div>
+          <div className="footer__content">
+            <p>Ein Projekt der Heilsarmee</p>
+            <a
+              className="footer__button button button_primary"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/thelimelightcollective/app/180105282031811/"
+            >
+              Spenden/Donate
+            </a>
+          </div>
+        </section>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
