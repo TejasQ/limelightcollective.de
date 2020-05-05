ac7927d6eca8008ab28c642b262af93e2cfa1ee5
Tejas Kumar <tejas@tejas.qa>
2020-05-05 13:35:29 +0200

    Community RWD

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,12 +4,16 @@ const Title = styled.h1<{ condensed?: boolean }>`
   position: relative;
   font-family: heading;
   font-style: italic;
-  font-size: 48px;
   font-weight: 300;
-  letter-spacing: ${({ condensed }) => (condensed ? "initial" : 16)}px;
+  letter-spacing: ${({ condensed }) => (condensed ? "initial" : 7)}px;
   text-transform: uppercase;
   z-index: 100;
   word-wrap: break-word;
+
+  @media (min-width: 768px) {
+    letter-spacing: ${({ condensed }) => (condensed ? "initial" : 16)}px;
+    font-size: 48px;
+  }
 `;
 
 export default Title;
--- pages/community.tsx
+++ pages/community.tsx
@@ -16,28 +16,49 @@ type CommunityProps = {
 };
 
 const LocalTitle = styled.h1`
-  font-size: 50px;
-  letter-spacing: 16px;
-  font-weight: normal;
   text-transform: uppercase;
-  margin: 0;
+  text-align: left;
+  padding: 0 16px;
+  margin: 55px 0 -40px;
+  letter-spacing: 6px;
+  font-weight: bold;
+  font-size: 23px;
+
+  @media (min-width: 768px) {
+    font-size: 50px;
+    letter-spacing: 16px;
+    font-weight: normal;
+    margin: 0;
+  }
 `;
 
 const LocalBall = styled.div`
+  margin: 0;
+  overflow: hidden;
+  padding: 30px;
   display: flex;
   place-content: center;
   place-items: center;
-  margin: 0 auto;
   border: 2px solid white;
   color: white;
+  overflow: auto;
   border-radius: 50%;
   z-index: 100;
   background-size: cover;
   background-position: center;
   background-image: url(/images/community-bg.jpg);
   transform: none;
-  grid-column: 2 / span 12;
-  grid-row: 2 / span 13;
+  grid-row: 11 / span 18;
+  grid-column: 2 / span 18;
+
+  @media (min-width: 768px) {
+    width: auto;
+    height: auto;
+    margin: 0 auto;
+    padding: 40px;
+    grid-column: 2 / span 12;
+    grid-row: 2 / span 13;
+  }
 
   @media (min-width: 1024px) {
     grid-column: 4 / span 12;
@@ -60,17 +81,24 @@ const Container = styled(Page)`
 
 const BallsContainer = styled.div`
   display: grid;
-  width: 1024px;
+  grid-template-columns: repeat(auto-fill, minmax(16px, 1fr));
+  grid-template-rows: repeat(auto-fill, minmax(16px, 1fr));
+  width: 320px;
   height: 100%;
   margin: 0 auto;
-  grid-template-columns: repeat(auto-fill, minmax(56px, 1fr));
-  grid-template-rows: repeat(auto-fill, minmax(50px, 1fr));
+
+  @media (min-width: 768px) {
+    width: 1024px;
+
+    grid-template-columns: repeat(auto-fill, minmax(56px, 1fr));
+    grid-template-rows: repeat(auto-fill, minmax(50px, 1fr));
+  }
 `;
 
 const Ball = styled.div`
   transition: transform 0.6s cubic-bezier(0.66, 0.19, 0.38, 0.99);
-  width: 170px;
-  height: 170px;
+  width: 120px;
+  height: 120px;
   display: flex;
   place-content: center;
   place-items: center;
@@ -86,6 +114,11 @@ const Ball = styled.div`
   backdrop-filter: blur(10px);
   overflow: visible;
 
+  @media (min-width: 768px) {
+    width: 170px;
+    height: 170px;
+  }
+
   cursor: pointer;
   :hover {
     transform: scale(1.1);
@@ -94,25 +127,40 @@ const Ball = styled.div`
 
 const ballRows = {
   "Get Involved": css`
-    grid-row: 15;
+    grid-row: 28;
+    @media (min-width: 768px) {
+      grid-row: 15;
+    }
     @media (min-width: 1024px) {
-      grid-row: 9;
+      grid-row: 11;
     }
   `,
   "Prayer Dance": css`
-    grid-column: 11;
-    grid-row: 15;
+    grid-column: 13;
+    grid-row: 29;
+    @media (min-width: 768px) {
+      grid-column: 11;
+      grid-row: 15;
+    }
     @media (min-width: 1024px) {
       grid-column: 16;
       grid-row: 11;
     }
   `,
   "Soul Space": css`
-    grid-row: 2;
+    grid-row: 3;
+
+    @media (min-width: 768px) {
+      grid-row: 2;
+    }
   `,
   "Our Community": css`
-    grid-row: 1;
-    grid-column: 11;
+    grid-row: 5;
+    grid-column: 14;
+    @media (min-width: 768px) {
+      grid-row: 1;
+      grid-column: 11;
+    }
     @media (min-width: 1024px) {
       grid-column: 16;
     }
@@ -121,57 +169,88 @@ const ballRows = {
 
 const ballImages = {
   "Our Community": {
-    style: {
-      width: "540px",
-      position: "absolute",
-      top: "-97px",
-      left: "-249px",
-      zIndex: 30,
-    },
+    style: css`
+      position: absolute;
+      z-index: 30;
+      width: 353px;
+
+      left: -163px;
+      top: -9px;
+
+      @media (min-width: 768px) {
+        width: 540px;
+        top: -97px;
+        left: -249px;
+      }
+    `,
     url: "/images/community-circle-2.png",
   },
   "Get Involved": {
     url: "/images/community-circle-5.png",
-    style: {
-      width: "578px",
-      position: "absolute",
-      left: "-120px",
-      top: "-189px",
-      zIndex: 30,
-    },
+    style: css`
+      position: absolute;
+      z-index: 30;
+      width: 353px;
+      left: -69px;
+      top: -49px;
+      @media (min-width: 768px) {
+        width: 578px;
+        left: -120px;
+        top: -183px;
+      }
+    `,
   },
   "Prayer Dance": {
     url: "/images/community-circle-4.png",
-    style: {
-      width: "560px",
-      position: "absolute",
-      top: "-176px",
-      left: "-227px",
-      zIndex: 30,
-    },
+    style: css`
+      position: absolute;
+      z-index: 30;
+      width: 353px;
+      left: -140px;
+      top: -50px;
+      @media (min-width: 768px) {
+        width: 560px;
+        top: -176px;
+        left: -227px;
+      }
+    `,
   },
   "Soul Space": {
     url: "/images/community-circle-1.png",
-    style: {
-      width: "500px",
-      position: "absolute",
-      top: "-120px",
-      left: "-180px",
-      zIndex: 30,
-    },
+    style: css`
+      position: absolute;
+      z-index: 30;
+      width: 353px;
+      top: -22px;
+      left: -128px;
+
+      @media (min-width: 768px) {
+        width: 560px;
+        top: -127px;
+        left: -204px;
+      }
+    `,
   },
 };
 
 const activeBallRows = {
   "Get Involved": css`
     ${ballRows["Get Involved"].styles}
-    transform: translate(100px, -50px) !important;
     animation: none !important;
+    transform: translate(30px, -30px) !important;
+
+    @media (min-width: 768px) {
+      transform: translate(100px, -50px) !important;
+    }
   `,
   "Prayer Dance": css`
     ${ballRows["Prayer Dance"].styles}
     animation: none !important;
-    transform: translate(-80px, -50px) !important;
+    transform: translate(-20px, -50px) !important;
+
+    @media (min-width: 768px) {
+      transform: translate(-80px, -50px) !important;
+    }
     @media (min-width: 1024px) {
       transform: translate(-160px, -50px) !important;
     }
@@ -179,7 +258,11 @@ const activeBallRows = {
   "Soul Space": css`
     ${ballRows["Soul Space"].styles}
     animation: none !important;
-    transform: translate(80px, 50px) !important;
+    transform: translate(20px, 80px) !important;
+
+    @media (min-width: 768px) {
+      transform: translate(80px, 50px) !important;
+    }
 
     @media (min-width: 1024) {
       transform: translate(100px, 90px) !important;
@@ -189,6 +272,10 @@ const activeBallRows = {
     ${ballRows["Our Community"].styles}
     animation: none !important;
     transform: translate(-20px, 40px) !important;
+
+    @media (min-width: 768px) {
+      transform: translate(-20px, 40px) !important;
+    }
 
     @media (min-width: 1024px) {
       transform: translate(-140px, 40px) !important;
@@ -201,13 +288,23 @@ const CommunityPage = ({ balls }: CommunityProps) => {
 
   return (
     <Container>
-      <br />
-      <br />
-      <br />
-      <LocalTitle>Community</LocalTitle>
-      <br />
-      <br />
-      <br />
+      <div
+        css={css`
+          line-height: 0px;
+          @media (min-width: 768px) {
+            display: block;
+            line-height: 1;
+          }
+        `}
+      >
+        <br />
+        <br />
+        <br />
+        <LocalTitle>Community</LocalTitle>
+        <br />
+        <br />
+        <br />
+      </div>
       <BallsContainer>
         {balls.map((b, i) => (
           <Ball
@@ -217,26 +314,48 @@ const CommunityPage = ({ balls }: CommunityProps) => {
             }}
             key={b.Name}
           >
-            <div style={{ width: 170, height: 170, position: "relative" }}>
+            <div
+              css={css`
+                position: relative;
+                width: 170px;
+                height: 170px;
+                pointer-events: none;
+              `}
+            >
               <span
-                style={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  width: "100%",
-                  height: "100%",
-                  transform: "translate(-50%,-50%)",
-                  zIndex: 50,
-                }}
+                css={css`
+                  position: absolute;
+                  top: 50%;
+                  left: 50%;
+                  width: 100%;
+                  height: 100%;
+                  transform: translate(-50%, -50%);
+                  z-index: 50;
+                  font-size: 21px;
+                  padding-top: 51px;
+
+                  @media (min-width: 768px) {
+                    font-size: 31px;
+                    padding-top: 0;
+                  }
+                `}
               >
                 {b.Name}
               </span>
-              <img alt="LOL" style={ballImages[b.Name].style} src={ballImages[b.Name].url} />
+              <img alt="LOL" css={ballImages[b.Name].style} src={ballImages[b.Name].url} />
             </div>
           </Ball>
         ))}
         <LocalBall>
-          <div style={{ textAlign: "center", maxWidth: "80%" }}>
+          <div
+            css={css`
+              height: 100%;
+              @media (min-width: 768px) {
+                text-align: center;
+                max-width: 80%;
+              }
+            `}
+          >
             <Title>{balls[currentBall].Name}</Title>
             <ReactMarkdown>{balls[currentBall].Content}</ReactMarkdown>
           </div>
